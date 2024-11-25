9ddb07cbe7e0ce9f53c8ca6abc7dd23516b4fb7d
Long <109840350+LongLong10203@users.noreply.github.com>
2024-11-25 20:45:37 +0800

    Disable cache

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from prisma import Prisma
 
 app = Flask(__name__)
 app.secret_key = uuid4().hex
+# disable cache
+app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
 
 prisma = Prisma()
 
@@ -143,18 +145,6 @@ async def delete_account():
     )
     await prisma.disconnect()
     return make_response(redirect("/logout"))
-
-@app.after_request
-def add_header(r):
-    """
-    Add headers to both force latest IE rendering engine or Chrome Frame,
-    and also to cache the rendered page for 10 minutes.
-    """
-    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    r.headers["Pragma"] = "no-cache"
-    r.headers["Expires"] = "0"
-    r.headers['Cache-Control'] = 'public, max-age=0'
-    return r
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3016, debug=True)
